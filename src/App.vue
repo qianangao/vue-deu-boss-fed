5b36171b9ca3ec75f407e614ed6548ed0e390fe7
gaoqi <1819561335@qq.com>
2021-04-21 16:59:35 +0800

    页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
   <div id="app">
-    <h1>app</h1>
-    <Button>aaa</Button>
     <router-view />
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-
+import request from '@/utils/request'
+request({
+  method: 'get',
+  url: '/boss/v2/api-docs?group=edu-boss-boot'
+}).then(res => {
+  console.log('res', res)
+})
 export default Vue.extend({
   data () {
     return {}
   },
-  methods: {
-
-  }
+  methods: {}
 })
-
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+// @import '~@/styles/variables.scss';
+.text {
+  color: $danger-color;
+}
+</style>
---
+++ src/layout/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="layout">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Layout"
+});
+</script>
+
+<style lang="scss" scoped></style>
