111530ec0a1a3ec9044393a0104dff04c9fb2d39
Your Name <you@example.com>
2021-07-22 16:45:18 +0800

    课程内容

--- src/services/section.ts
+++ src/services/section.ts
@@ -10,3 +10,20 @@ export const getSectionAndLesson = (courseId: string | number) => {
         }
     })
 }
+
+// 保存更新章节内容
+export const saveOrUpdateSection = (data: any) => {
+    return request({
+        method: 'post',
+        url: '/boss/course/section/saveOrUpdateSection',
+        data
+    })
+}
+// 保存更新课时
+export const saveOrUpdateLession = (data: any) => {
+    return request({
+        method: 'post',
+        url: '/boss/course/lesson/saveOrUpdate',
+        data
+    })
+}
--- src/views/course/components/section.vue
+++ src/views/course/components/section.vue
@@ -4,7 +4,9 @@
       <el-tree
       draggable
       :data="data"
-      :props="defaultProps">
+      :props="defaultProps"
+      :allow-drop="allowDrop"
+      @node-drop="dropSuccess">
       <div slot-scope="{node, data}" class="inner">
           <span>{{node.label}}</span>
           <span v-if="data.sectionName">
@@ -24,7 +26,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { getSectionAndLesson } from '@/services/section.ts'
+import { getSectionAndLesson, saveOrUpdateLession, saveOrUpdateSection } from '@/services/section.ts'
 export default Vue.extend({
   name: 'course-section',
    props: {
@@ -51,7 +53,32 @@ export default Vue.extend({
       async loadSectionAndLession () {
           const { data } = await getSectionAndLesson(this.courseId)
           this.data = data.data
-          console.log(data);
+      },
+      allowDrop (draggingNode: any, dropNode: any, type: any) {
+          // draggingNode 拖动的节点
+          // dropNode 放置的目标节点
+          // type ：'prev'、'inner' 和 'next'，分别表示放置在目标节点前、插入至目标节点和放置在目标节点后
+          return type !== 'inner' && draggingNode.data.sectionId === dropNode.data.sectionId
+      },
+      async dropSuccess (draggingNode: any, dropNode: any, type: any, event: any) {
+           try {
+               await Promise.all(dropNode.parent.childNodes.map((item: any, index: number) => {
+               if (draggingNode.data.lessonDTOS) {
+                   return saveOrUpdateSection({
+                       id: item.data.id,
+                       orderNum: index + 1
+                   })
+               } else {
+                   return saveOrUpdateLession({
+                       id: item.data.id,
+                       orderNum: index + 1
+                   })
+               }
+           }))
+           this.$message.success('排序成功')
+           } catch (error) {
+               this.$message.success('排序失败')
+           }
       }
   }
 })
